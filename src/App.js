82dda2e9e0bc9a92f2cd7d8d7763fdff55e936d7
Laura Capella <87069245+lauracapella@users.noreply.github.com>
2022-06-03 13:32:36 +0200

    login localstorage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 
 import React, { useEffect, useState } from 'react';
 import Routes from "./application/routes";
+import axios from "axios"
 
 
 export default function App() {
 
   const [spaceShipsData, setSpaceShipsData] = useState([]);
+  const [currentStarship, setCurrentStarship] = useState({});
+/*   const [detalleNave, setDetalleNave] = useState();
+ */  const [count, setCount] = useState(1)
 
+  const [username, setUsername] = useState("user1")
+  const [password, setPassword] = useState("pass")
+  const [user, setUser] = useState()
 
+ /*  const handleSubmit = async e => {
+    e.preventDefault();
+    const user ={username, password};
+    const response = await axios.post('', user);
+    setUser(response.data)
+    localStorage.setItem('user', response.data);
+    console.log(response.data)
+    
+  }; */
 
- const [currentStarship, setCurrentStarship] = useState({});
-
-  const [detalleNave, setDetalleNave] = useState();
-  const [count, setCount] = useState(1)
-
+  const handleSubmit = () => {
+    localStorage.setItem('Name', username);
+    localStorage.setItem('Password', password);
+ };
  
-
+ 
   useEffect(() => {
     fetch("https://swapi.dev/api/starships/?page=" + count)
       .then(resp => resp.json())
@@ -23,9 +38,6 @@ export default function App() {
         setSpaceShipsData(resp.results)
       })
   }, [count]);
-
-
-
 
   const moreSpaceshipList = () => {
     if (count >= 1 && count <= 3) {
@@ -35,8 +47,13 @@ export default function App() {
     }
   }
 
+  if (user) {
+    return <div>{user.name} is logged in</div>
+  }
+
   return (
     <>
+    
                
       <Routes 
         spaceShipsData={spaceShipsData}
@@ -44,6 +61,26 @@ export default function App() {
         spaceShip={currentStarship} 
         moreSpaceshipList={moreSpaceshipList} >
       </Routes>
+
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username: </label>
+      <input
+        type="text"
+        value={username}
+        placeholder="enter a username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+      <div>
+        <label htmlFor="password">password: </label>
+        <input
+          type="password"
+          value={password}
+          placeholder="enter a password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
       
     </>
   )
